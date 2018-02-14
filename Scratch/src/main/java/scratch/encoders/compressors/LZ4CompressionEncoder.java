1c8ccf6148e001f1c8898a54a3e34a38388b315f
Nathaniel Sherry <nsherry4@uwo.ca>
2018-02-14 15:08:20 -0500

    Added FileBacked class for storing a single item to a file. Remvoed some out of date documentation

--- Scratch/src/main/java/scratch/encoders/compressors/LZ4CompressionEncoder.java
+++ Scratch/src/main/java/scratch/encoders/compressors/LZ4CompressionEncoder.java
@@ -1,41 +1,10 @@
 package scratch.encoders.compressors;
 
-import java.awt.Color;
-import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.util.ArrayList;
-import java.util.List;
-
 import net.jpountz.lz4.LZ4Compressor;
 import net.jpountz.lz4.LZ4Factory;
 import net.jpountz.lz4.LZ4SafeDecompressor;
-import scitypes.ISpectrum;
-import scitypes.SparsedList;
-import scitypes.Spectrum;
 import scratch.ScratchEncoder;
-import scratch.ScratchList;
 
-/**
- * SpectrumList is an implementation of the List interface which writes 
- * out values to a temporary file, rather than storing elements in memory.
- * This is useful for lists which are sufficiently large to cause memory
- * concerns in a typical JVM.
- * <br /><br /> 
- * Storing elements on disk means that get operations will return copies 
- * of the objects in the list rather than the originally stored objects. 
- * If an element is retrieved from the list, modified, and then 
- * retrieved a second time, the second copy retrieved will lack the 
- * modifications made to the first copy.
- * 
- * To create a new SpectrumList, call {@link LZ4CompressionEncoder#create(String)}.
- * If the SpectrumList cannot be created for whatever reason, a memory-based
- * list will be created instead.
- * <br/><br/>
- * Note that this class depends on the specific implementation of Spectrum
- * being {@link ISpectrum} 
- * @author Nathaniel Sherry, 2011-2012
- *
- */
 
 public class LZ4CompressionEncoder implements ScratchEncoder<byte[]>{
 
--- Scratch/src/main/java/scratch/single/Compressed.java
+++ Scratch/src/main/java/scratch/single/Compressed.java
@@ -9,10 +9,10 @@ import scratch.encoders.compressors.DeflateCompressionEncoder;
 import scratch.encoders.serializers.Serializers;
 import scratch.encoders.serializers.SerializingEncoder;
 
-public class Compressed<T> {
+public class Compressed<T> implements ByteStorage<T> {
 
-	private byte[] data;
-	private ScratchEncoder<T> encoder;
+	byte[] data;
+	ScratchEncoder<T> encoder;
 	
 	public static <T extends Serializable> Compressed<T> create(T value) {
 		return create(value, new CompoundEncoder<T>(Serializers.java(), Compressors.deflate()));
@@ -24,12 +24,20 @@ public class Compressed<T> {
 		c.encoder = encoder;
 		return c;
 	}
+	
+	@Override
 	public T get() {
 		return this.encoder.decode(this.data);
 	}
 	
+	@Override
 	public byte[] getBytes() {
 		return data;
+	}
+
+	@Override
+	public ScratchEncoder<T> getEncoder() {
+		return encoder;
 	}
 	
 }
