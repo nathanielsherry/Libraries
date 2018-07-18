153492e65463dc73bec251fbf52e8b6ad18a6157
Nathaniel Sherry <nsherry4@uwo.ca>
2018-07-18 10:57:30 -0400

    Updated Apps.browser in Envi to use the java.awt.Desktop class directly, instead of through reflection

--- CommonEnvironment/src/main/java/commonenvironment/Apps.java
+++ CommonEnvironment/src/main/java/commonenvironment/Apps.java
@@ -1,7 +1,9 @@
 package commonenvironment;
 
+import java.awt.Desktop;
 import java.io.IOException;
-import java.lang.reflect.Method;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 
 
@@ -10,30 +12,16 @@ public class Apps
 
 	public static void browser(String _url)
 	{
-		
-		String url = _url.toLowerCase().startsWith("http://") ? _url : "http://" + _url;
-		
-		
+		//Check for http instead of http:// so that we don't add it to https:// urls
+		String url = _url.toLowerCase().startsWith("http") ? _url : "http://" + _url;
 		
 		try {
-						
-			Class<?> desktop = Class.forName("java.awt.Desktop");
-			
-			Method browse = desktop.getDeclaredMethod("browse", new Class[] {java.net.URI.class});
-			Method getDesktop = desktop.getDeclaredMethod("getDesktop");
-			
-			Object[] urlobjs = new Object[] {java.net.URI.create(url)};
-			
-			browse.invoke(getDesktop.invoke(null), urlobjs);
-			
-			
-		} catch (Exception e)
-		{
-						
-			//this must be java 5 or earlier
+			Desktop desktop = Desktop.getDesktop();
+			desktop.browse(new URI(_url));
+		} catch (UnsupportedOperationException | IOException | URISyntaxException e1) {
 			openDocument(url);
-			
 		}
+
 		
 	}
 	
