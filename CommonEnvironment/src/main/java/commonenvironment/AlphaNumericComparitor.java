bc0da22a9e1dfc51dfcb9b8f826779c2b77024cc
Nathaniel Sherry <nsherry4@uwo.ca>
2018-08-02 16:28:44 -0400

    Bugfix where AlphaNumericComparitor wouldn't check existing numercial state buffers at end of string

--- CommonEnvironment/src/main/java/commonenvironment/AlphaNumericComparitor.java
+++ CommonEnvironment/src/main/java/commonenvironment/AlphaNumericComparitor.java
@@ -1,4 +1,4 @@
- package commonenvironment;
+package commonenvironment;
 
 import java.util.Comparator;
 
@@ -51,7 +51,6 @@ public class AlphaNumericComparitor implements Comparator<String> {
 		char char0, char1;
 		boolean char0IsDigit, char1IsDigit;
 		boolean numericMode = false;
-		long number0, number1;
 		
 		//go over these two strings one character at a time
 		for (int i = 0; i < length; i++){
@@ -106,31 +105,29 @@ public class AlphaNumericComparitor implements Comparator<String> {
 			} else if (char0IsDigit && char1IsDigit){
 				//both are digits - enter (or stay in) numeric mode to get the whole numbers from each string
 				
-				if (numericMode){
-					buffer0 += char0;
-					buffer1 += char1;
-				} else {
-					buffer0 = "" + char0;
-					buffer1 = "" + char1;
+				//if we're not in numeric mode, enter it now.
+				if (!numericMode) {
+					numericMode = true;
+					buffer0 = "";
+					buffer1 = "";
 				}
 				
-				numericMode = true;
-				
+				buffer0 += char0;
+				buffer1 += char1;
+
 			} else {
 				//they are both not digits
 				
 				if (numericMode){
 					
 					//we were in numeric mode, and both numbers are the same number of digits long
-					number0 = Long.parseLong(buffer0);
-					number1 = Long.parseLong(buffer1);
+					int cmp = compareNumericStrings(buffer0, buffer1);
+					//reset the buffers
+					buffer0 = "";
+					buffer1 = "";
 					
-					if (number0 == number1){
-						//these numbers are the same
-					} else {
-						//these numbers are not the same, so we return the difference.
-						//given 3 and 7, 3 is less than 7, so we return 3-7=-4 which is <0 so is 'less than'
-						return (int)(number0-number1);
+					if (cmp != 0) {
+						return cmp;
 					}
 					
 					numericMode = false;
@@ -149,9 +146,25 @@ public class AlphaNumericComparitor implements Comparator<String> {
 			
 		} //for each character in strings
 		
-		//looks like they're the same after all
-		return 0;
+		//looks like we've reached the end of the loop without finding a difference.
+		//BUT, there may still be characters in the numeric mode buffers
+		return compareNumericStrings(buffer0, buffer1);
 		
+	}
+	
+	private int compareNumericStrings(String s0, String s1) {
+		//we were in numeric mode, and both numbers are the same number of digits long
+		long number0 = Long.parseLong(s0);
+		long number1 = Long.parseLong(s1);
+		
+		if (number0 == number1){
+			//these numbers are the same
+			return 0;
+		} else {
+			//these numbers are not the same, so we return the difference.
+			//given 3 and 7, 3 is less than 7, so we return 3-7=-4 which is <0 so is 'less than'
+			return (int)(number0-number1);
+		}
 	}
 
 }
